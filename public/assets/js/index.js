138cf6df3163009b6d7a08980c6d46dfb58f2158
isabeljoseph <igbojionuisabella@gmail.com>
2019-09-19 18:09:53 +0100

    made changes to index.js

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,6 +1,37 @@
 $(document).ready(function () {
-	
+	var id = "";
 	getEmployees();
+
+	$.ajax({
+		url: 'http://localhost:3000/employees?name=' + $('#search-box').val(),
+		method: 'GET',
+		dataType: 'json',
+		success: function (data) {
+			$(data).each(function (i, employee) {
+				$('#employeeBody').append($("<tr>")
+					.append($("<td>").append(employee.id))
+					.append($("<td>").append(employee.name))
+					.append($("<td>").append(employee.bank_name))
+					.append($("<td>").append(employee.account_number))
+					.append($("<td>").append(employee.qualification))
+					.append($("<td>").append(employee.designation))
+					.append($("<td>").append(employee.level))
+					.append($("<td>").append(`${employee.salary}`))
+					.append($("<td>").append(employee.payment_status))
+					.append($("<td>").append(`
+				<i class = "far fa-edit editEmp" data-empid="` + employee.id + `"></i>
+				<i class = "fas fa-trash deleteEmp" data-empid="` + employee.id + `"></i>
+									`)));
+			});
+			loadButtons();
+		},
+		error: function () {
+			alert('error adding employee')
+		}
+
+	});
+
+
 	$("#newEmpBtn").on("click", function (e) {
 		// toggle new employee buttons
 		$("#newForm").toggle();
@@ -12,9 +43,6 @@ $(document).ready(function () {
 			url: 'http://localhost:3000/employees',
 			method: 'GET',
 			dataType: 'json',
-			data: {
-				test: 'test data'
-			},
 			success: function (data) {
 				$(data).each(function (i, employee) {
 					$('#employeeBody').append($("<tr>")
@@ -39,35 +67,37 @@ $(document).ready(function () {
 			}
 		});
 	}
-	function searchEmployee(){
-		$("#search-box").on("keyup",function(){
-			var value = $(this).val().toLowerCase();
-			$("#employeeBody").filter(function(){
-				$(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-			});
-		});
-	}
+	// search one employee
+
+	$("#searchEmpBtn").on("click", function () {
+
+		$("#searchForm").toggle();
+		$('#employeeTable').toggleClass("hide");
+
+	});
+
 	// Get one employee
 	function getOneEmployee(num) {
-	
+		$("#updateForm").show();
+		id = num;
 		$.ajax({
-			url: 'http://localhost:3000/employees/' +num,
+			url: 'http://localhost:3000/employees/' + num,
 			method: 'GET',
 			dataType: 'json',
 			success: function (data) {
 				$($("#updateForm")[0].updateEmpId).val(data.id);
-				$($("updateForm")[0].updateName).val(data.name);
-				$($("updateForm")[0].updateBank).val(data.bank_name);
-				$($("updateForm")[0].updateAcct).val(data.account_number);
-				$($("updateForm")[0].updateDes).val(data.designation);
-				$($("updateForm")[0].updateLevel).val(data.level);
-				$($("updateForm")[0].updateSalary).val(data.salary);
-				$($("updateForm")[0].updatePayStat).val(data.payment_status);
-				$("#updateForm").show();
+				$($("#updateForm")[0].updateName).val(data.name);
+				$($("#updateForm")[0].updateBank).val(data.bank_name);
+				$($("#updateForm")[0].updateAcct).val(data.account_number);
+				$($("#updateForm")[0].updateDes).val(data.designation);
+				$($("#updateForm")[0].updateLevel).val(data.level);
+				$($("#updateForm")[0].updateSalary).val(data.salary);
+				$($("#updateForm")[0].updatePayStat).val(data.payment_status);
+
 
 			},
 			error: function () {
-				alert('error getting one employee')
+				//alert('error getting one employee')
 			}
 		});
 	}
@@ -78,7 +108,7 @@ $(document).ready(function () {
 			bank_name: $($("#newForm")[0].bank).val(),
 			account_number: $($("#newForm")[0].acct).val(),
 			qualification: $($("#newForm")[0].qualif).val(),
-			designation:$($("#newForm")[0].des).val(),
+			designation: $($("#newForm")[0].des).val(),
 			level: $($("#newForm")[0].level).val(),
 			salary: $($("#newForm")[0].salary).val(),
 			payment_status: $($("#newForm")[0].payStat).val()
@@ -86,11 +116,13 @@ $(document).ready(function () {
 		postEmployee(data);
 		$("#newForm").trigger("reset");
 		$("#newForm").toggle();
+		getEmployees()
+		$('#employeeTable').css("display", "block");
 		e.preventDefault();
 	});
 
 	function postEmployee(data) {
-		
+
 		$.ajax({
 			url: 'http://localhost:3000/employees',
 			method: 'POST',
@@ -110,7 +142,8 @@ $(document).ready(function () {
 	// function for loading buttons and attaching it to each employee's id
 	function loadButtons() {
 		$(".editEmp").click(function (e) {
-			getOneEmployee($($(this)[0]).data("empId"));
+			//$('#updateForm').show();
+			getOneEmployee($($(this)[0]).data("empid"));
 			e.preventDefault();
 		});
 		$(".deleteEmp").click(function (e) {
@@ -118,39 +151,44 @@ $(document).ready(function () {
 			e.preventDefault();
 		});
 	}
+
+
 	//update employee function
-	function putEmployee(num, data) {
+	function putEmployee(num) {
 		num = $("#updateEmpId").val();
+
+	}
+	$("#updateEmployee").on("click", function (e) {
+		e.preventDefault();
+		let data = {
+			name: $($("#updateForm")[0].updateName).val(),
+			bank_name: $($("#updateForm")[0].updateBank).val(),
+			account_number: $($("#updateForm")[0].updateAcct).val(),
+			qualification: $($("#updateForm")[0].updateQualif).val(),
+			designation: $($("#updateDes")[0].updateDes).val(),
+			level: $($("#updateForm")[0].updateLevel).val(),
+			salary: $($("#updateForm")[0].updateSalary).val(),
+			payment_status: $($("#updateForm")[0].updatePayStat).val()
+		}
+		//putEmployee($($("#updateForm")[0].empId).val());
+
 		$.ajax({
-			url: 'http://localhost:3000/employees/' +num,
-			method: 'PUT',
+			url: 'http://localhost:3000/employees/' + id,
+			method: 'PATCH',
 			dataType: 'json',
 			data: data,
 			success: function (data) {
-				console.log(data);
+				alert(data);
 				getEmployees();
 			},
 			error: function () {
 				alert('error updating employee');
 			}
 		});
-	}
-	$("#updateEmployee").on("click",function(e){
-		let data = {
-			id: $($("#updateForm")[0].updateEmpId).val(),
-			name: $($("#updateForm")[0].updateName).val(),
-			bank_name: $($("#updateForm")[0].updateBank).val(),
-			account_number: $($("#updateForm")[0].updateAcct).val(),
-			qualification: $($("#updateForm")[0].updateQualif).val(),
-			designation:$($("#updateDes")[0].updateDes).val(),
-			level: $($("#updateForm")[0].updateLevel).val(),
-			salary: $($("#updateForm")[0].updateSalary).val(),
-			payment_status: $($("#updateForm")[0].updatePayStat).val()
-		}
-		putEmployee($($("#updateForm")[0].empId).val(),data);
 		$("#updateForm").trigger("reset");
 		$("#updateForm").toggle();
 	});
+
 	function deleteEmployee(num) {
 		$.ajax({
 			url: 'http://localhost:3000/employees/' + num,
@@ -161,7 +199,7 @@ $(document).ready(function () {
 				console.log(data);
 				getEmployees();
 			},
-			error:function(){
+			error: function () {
 				alert('error deleting employee');
 			}
 
